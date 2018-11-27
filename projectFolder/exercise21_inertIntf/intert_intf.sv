55fe63df54a49dacbd547f6cf04bd0433da9f6ea
Kevin D. Wilson <kwilson851@gatech.edu>
2018-11-27 17:05:41 -0600

    comments on intert_intf

--- projectFolder/exercise21_inertIntf/intert_intf.sv
+++ projectFolder/exercise21_inertIntf/intert_intf.sv
@@ -5,12 +5,12 @@ module intert_intf(clk, rst_n,  vld, ptch, SS_n, SCLK, MOSI, MISO, INT);
 	input  INT;							// Interrupt signal from intertial sensor, informs new measurement ready to be read
 	
 	output logic vld;					// Asserted from SM. Consumed in intertial_integrator. Also used in balance_ctrl
-	output [15:0] ptch;					// Primary output. Fusion corrected ptch from Segway
+	output [15:0] ptch;					// Primary output. Fusion corrected ptch from Segway -
 	
 	
 	//SPI interface to inertial sensor
 	input MISO;
-	output SS_n, SCLK, MOSI;
+	output SS_n, SCLK, MOSI; 
 	
 	
 	
