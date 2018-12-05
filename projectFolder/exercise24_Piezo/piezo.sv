5e3394371a0a74459ceab5cb9d24923800d9d040
tmiller34 <tmiller34@ad.engr.wisc.edu>
2018-12-05 10:09:08 -0600

    further modded piezo

--- projectFolder/exercise24_Piezo/piezo.sv
+++ projectFolder/exercise24_Piezo/piezo.sv
@@ -11,13 +11,14 @@ module piezo(clk, rst_n, norm_mode, ovr_spd, batt_low, piezo, piezo_n);
 	wire norm_tone_dur,warn_tone_dur;// controls the duration of buzzer
 					 // pulses when in normal or danger modes
 
-	always_ff @(posedge clk)
+	always_ff @(posedge clk,negedge rst_n)
 	  if (!rst_n) timer <= 0;
 	  else timer <= timer + 1;
 
-	assign norm_mode_wave = timer[15];	// bit 16 of timer gives ~1.5kHz wave
-	assign ovr_spd_wave = timer[13];	// ~6.1kHz wave
-	assign batt_low_wave = timer[14];	// ~3kHz wave
+	assign norm_mode_wave = timer[16];	// bit 16 of timer gives ~750Hz wave
+	assign ovr_spd_wave = timer[14];	// ~3kHz wave
+	assign batt_low_wave = timer[15];	// ~1.5kHz wave
+	assign ovrSpd_battLow_wave = timer[13];	// ~6.1kHz wave
 
 	assign norm_tone_dur = &timer[25:24];	// buzzer duration when in normal operation
 						// mode (goes for the last .34 sec of 1.34 sec
@@ -28,7 +29,7 @@ module piezo(clk, rst_n, norm_mode, ovr_spd, batt_low, piezo, piezo_n);
 
 
 	//// Assigning piezo and it's complement ////
-	assign piezo = (ovr_spd & batt_low) ? ((ovr_spd_wave | batt_low_wave) & warn_tone_dur) :
+	assign piezo = (ovr_spd & batt_low) ? (ovrSpd_battLow_wave & warn_tone_dur):
 		       ovr_spd	 ? (ovr_spd_wave & warn_tone_dur)  :
 		       batt_low	 ? (batt_low_wave & norm_tone_dur) :
 		       norm_mode ? (norm_mode_wave & norm_tone_dur):
